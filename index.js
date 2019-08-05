576d43deca2165fd5578e5319cae7aa3b3e981f8
Eduardo Gonzalez <eg9800@gmail.com>
2019-08-04 23:00:38 -0700

    added more function to js file, fetching of data works, next function is to display the data

--- index.js
+++ index.js
@@ -1,4 +1,57 @@
 'use strict';
 
-  const searchURL: 'https://developer.nps.gov/api/v1/parks?parkCode=acad';
-  const apiKey: 'spSzwyMegX2P7akcLWa88v3SMHBtdZmw5defzeIx';
+  const searchURL= 'https://developer.nps.gov/api/v1/parks';
+  
+  const apiKey= 'spSzwyMegX2P7akcLWa88v3SMHBtdZmw5defzeIx';
+
+  // function displayResults(responsejson){
+  //   $('#results-list').empty();
+    
+  //   for (let i = 0; i < responsejson.)
+  // }
+
+  function formatQueryParams(params){
+    const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+  }
+
+  function getNPSdata (query, maxResults=10){
+    const params = {
+      stateCode: query,
+      limit: maxResults,
+      api_key: apiKey,
+    };
+    const queryString = formatQueryParams(params);
+    const url = searchURL + '?' + queryString;
+    console.log(url);
+
+    fetch(url)
+      .then(response => {
+        if(response.ok){
+          return response.json();
+        }
+        throw new Error
+        (response.statusText);
+      })
+      .then(responsejson =>
+        displayResults(responsejson));
+      console.log(responsejson);
+      // .catch(err => {
+      //   alert(err.message);
+      // })
+
+  }
+
+  function submitForm () {
+    $('form').submit(event => {
+
+      event.preventDefault();
+      const stateSearch = $('#state-search').val();
+        //alert('search clicked')
+      const results = $('#numResults').val();
+        //console.log(results, stateSearch);
+      getNPSdata(stateSearch, results);
+    }   
+  )}
+
+  $(submitForm)
